5f895f71f658c415e59339ba5dfbc76579aae648
italo <italomiranda67@gmail.com>
2022-08-17 16:39:51 -0300

    Feito views para livros/detalhar/listagem

--- emprestimos/views.py
+++ emprestimos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, DeleteView
+from .models import Livros, Autor, Emprestimo
 # Create your views here.
+
+class LivrosListView(ListView):
+    model = Livros
+    context_object_name = 'livros'
+    template_name = 'livros/listar.html'
+    
+class LivrosDetailView(DetailView):
+    model = Livros
+    context_object_name = 'livros'
+    template_name = 'livros/detalhe.html'
